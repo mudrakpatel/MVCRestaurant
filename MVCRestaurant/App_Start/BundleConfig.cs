d100d357e274d0b8a19bfce639b183f92a7d7f18
mudrakpatel <mudrakpatel96@gmail.com>
2016-11-19 02:24:08 -0600

    Index.cshtml page completed and added icon for website

--- MVCRestaurant/App_Start/BundleConfig.cs
+++ MVCRestaurant/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace MVCRestaurant
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.IgnoreList.Clear();
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -25,6 +27,7 @@ namespace MVCRestaurant
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
